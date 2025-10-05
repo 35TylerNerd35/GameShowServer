471136f02535b03876e513bf2c66443128bf2e2f
Tye <tylerc35@hotmail.co.uk>
2025-10-05 14:51:09 +0100

    New Table

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ const SUPABASE_URL = 'https://izghwhppfuumvzjehfrr.supabase.co'
 const SUPABASE_ANON_KEY = "sb_publishable_OfDnBziyMsBman1rO6HAuQ_5Oe1uBAf";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const table = "PollVotes";
 
-const options = ["Unreal", "Unity", "Godot"];
+// const options = ["Unreal", "Unity", "Godot"];
+let options = [];
 const optionsDiv = document.getElementById("options");
 const resultsDiv = document.getElementById("results");
+
+async function fetchOptions() {
+  const { data } = await supabase.from(table).select("option_name");
+  options = data.map(opt => opt.name);
+}
+
 
 // Render vote buttons
 options.forEach((opt, i) => {
   const btn = document.createElement("button");
   btn.textContent = opt;
   btn.onclick = async () => {
-    await supabase.from("poll_votes").insert([{ option_index: i }]);
+    await supabase.from(table).insert([{ option_index: i }]);
   };
   optionsDiv.appendChild(btn);
 });
 
 async function fetchVotes() {
-  const { data } = await supabase.from("poll_votes").select("*");
+  const { data } = await supabase.from(table).select("*");
   renderResults(data);
 }
 
@@ -39,12 +47,14 @@ supabase
   .channel('table-db-changes')
   .on(
     'postgres_changes',
-    { event: 'INSERT', schema: 'public', table: 'poll_votes' },
+    { event: 'INSERT', schema: 'public', table: 'PollVotes' },
     (payload) => {
+      fetchOptions();
       fetchVotes();
     }
   )
   .subscribe();
 
 // Initial fetch
+fetchOptions();
 fetchVotes();
