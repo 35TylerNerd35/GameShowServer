6560fb81549b1ac2a619fcc10f8be5b0926de233
Tye <tylerc35@hotmail.co.uk>
2025-10-05 15:13:27 +0100

    Updates instead of inserts

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const SUPABASE_URL = 'https://izghwhppfuumvzjehfrr.supabase.co'
 const SUPABASE_ANON_KEY = "sb_publishable_OfDnBziyMsBman1rO6HAuQ_5Oe1uBAf";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const buttons = {};
 const table = "PollVotes";
 
 // const options = ["Unreal", "Unity", "Godot"];
@@ -14,15 +15,19 @@ const resultsDiv = document.getElementById("results");
 async function fetchOptions() {
   const { data } = await supabase.from(table).select("option_name");
   options = data.map(opt => opt.name);
+  print(options)
 }
 
 
 // Render vote buttons
 options.forEach((opt, i) => {
   const btn = document.createElement("button");
+  buttons[i] = btn;
   btn.textContent = opt;
   btn.onclick = async () => {
-    await supabase.from(table).insert([{ option_index: i }]);
+    await OnButtonClick(btn);
+    // await supabase.from(table).insert([{ option_index: i }]);
+    // await supabase.from(table).update({ votes: })
   };
   optionsDiv.appendChild(btn);
 });
@@ -30,6 +35,12 @@ options.forEach((opt, i) => {
 async function fetchVotes() {
   const { data } = await supabase.from(table).select("*");
   renderResults(data);
+}
+
+async function OnButtonClick(btn) {
+  const buttonID = buttons.indexOf(btn);
+  const currentVotes = await supabase.from(table).select("votes").eq("option_id",  buttonID);
+  await supabase.from(table).update({ votes: currentVotes + 1 }).eq("option_id", buttonID);
 }
 
 function renderResults(data) {
