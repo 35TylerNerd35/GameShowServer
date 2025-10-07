3b49ace8338afe162f91f3e61fe2423a5e45348b
Tyler Leonard <tylerc35@hotmail.co.uk>
2025-10-07 14:59:20 +0100

    Testing Responses

--- api/clearTable.js
+++ api/clearTable.js
@@ -6,5 +6,7 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
-  await supabase.from("PollVotes").delete().neq('votes', -100);
+  // await supabase.from("PollVotes").delete().neq('votes', -100);
+  const { data } = await supabase.from("PollVotes").select("*");
+  res.status(200).json(data);
 }
