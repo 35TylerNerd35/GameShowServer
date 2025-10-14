70f08f57fc16fef842c91277aa3137c0967f6295
Tyler Leonard <tylerc35@hotmail.co.uk>
2025-10-14 19:02:50 +0100

    Setup Lobby API Gateway

---
+++ api/setupLobby.js
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+  if (req.method != "POST") return res.status(405).send("Method not allowed, please use POST");
+
+  // Grab lobby code from payload
+  const payload = req.body;
+  const lobbyCode = payload.lobbyCode;
+  
+  const { data, error } = await supabase.from("DeviceInformation").insert({ device_id : 0, lobby_id : lobbyCode, is_host : true, is_in_person : false });
+  res.status(200).json(data[0], error);
+}
