79fd5555a2dc0ea665bbbac2755397cb2a4a74fc
Tyler Leonard <tylerc35@hotmail.co.uk>
2025-10-14 17:49:22 +0100

    Responds with Data not Paylaod

--- api/deleteUser.js
+++ api/deleteUser.js
@@ -11,5 +11,5 @@ export default async function handler(req, res) {
   const deviceID = payload.device_id;
 
   const { data, error } = await supabase.from("DeviceInformation").delete().eq('device_id', deviceID);
-  res.status(200).json(payload, error);
+  res.status(200).json(data, error);
 }
