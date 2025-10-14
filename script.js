79c8d5bbc23dd3f46752fc3066de15c196d3e982
Tyler Leonard <tylerc35@hotmail.co.uk>
2025-10-14 14:50:56 +0100

    New Format for Subscriptions

--- script.js
+++ script.js
@@ -184,52 +184,76 @@ async function DisplayNewVote(option, increment) {
 }
 
 
+const channel = supabase
+    .channel('schema-db-changes')
+    .on(
+    'postgres_changes',
+    {
+        event: 'INSERT',
+        schema: 'public',
+    },
+    (payload) => console.log(payload)
+    )
+.subscribe()
 
+const channel2 = supabase
+  .channel('schema-db-changes')
+  .on(
+    'postgres_changes',
+    {
+      event: 'UPDATE',
+      schema: 'public',
+    },
+    (payload) => console.log(payload)
+  )
+.subscribe()
 
 
 Setup();
 
 // Subscribe to deletes
-supabase
-.channel('table-db-changes')
-.on(
-    'postgres_changes',
-    { event: 'DELETE', schema: 'public', table: voteTable },
-    (payload) => {
-        console.log("DELETE " + payload.old.option_name + " : " + lobbyCode);
-        if (payload.old.lobby_id != lobbyCode) {
-            return;
-        }
+// supabase
+// .channel('table-db-changes')
+// .on(
+//     'postgres_changes',
+//     { event: 'DELETE', schema: 'public', table: voteTable },
+//     (payload) => {
+//         console.log("DELETE " + payload.old.option_name + " : " + lobbyCode);
+//         if (payload.old.lobby_id != lobbyCode) {
+//             return;
+//         }
 
-        // Find element in array
-        const oldLobby = poll_options.find(element => element.option_id == payload.old.option_id);
-        const index = poll_options.indexOf(oldLobby);
-        document.getElementById(oldLobby.option_name + "Checkbox").remove();
-        document.getElementById(oldLobby.option_name + "Label").parentNode.remove();
+//         // Find element in array
+//         const oldLobby = poll_options.find(element => element.option_id == payload.old.option_id);
+//         const index = poll_options.indexOf(oldLobby);
+//         document.getElementById(oldLobby.option_name + "Checkbox").remove();
+//         document.getElementById(oldLobby.option_name + "Label").parentNode.remove();
 
-        // Remove from array
-        poll_options.splice(index, 1);
-    }
-)
-.subscribe();
+//         // Remove from array
+//         poll_options.splice(index, 1);
+//     }
+// )
+// .subscribe();
 
-// Subscribe to row inserts
-supabase
-.channel('table-db-changes')
-.on(
-    'postgres_changes',
-    { event: 'INSERT', schema: 'public', table: voteTable },
-    (payload) => {
-        console.log("INSERT " + payload.new.option_name + " : " + lobbyCode);
-        if (payload.new.lobby_id != lobbyCode) {
-            return;
-        }
+// // Subscribe to row inserts
+// supabase
+// .channel('table-db-changes')
+// .on(
+//     'postgres_changes',
+//     { event: 'INSERT', schema: 'public', table: voteTable },
+//     (payload) => {
+//         console.log("INSERT " + payload.new.option_name + " : " + lobbyCode);
+//         if (payload.new.lobby_id != lobbyCode) {
+//             return;
+//         }
 
-        poll_options.push(payload.new);
-        CreateButton(payload.new);
-    }
-)
-.subscribe();
+//         poll_options.push(payload.new);
+//         CreateButton(payload.new);
+//     }
+// )
+// .subscribe();
+
+
 
 
 
