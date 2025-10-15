1ce9c675f6bd4d54148ae9a28031711876a7021d
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-10-15 15:15:23 +0100

    Added API to Grab Rows from Table

--- api/grabRows.js
+++ api/grabRows.js
@@ -8,14 +8,10 @@ const supabase = createClient(
 export default async function handler(req, res) {
   if (req.method != "POST") return res.status(405).send("Method not allowed, please use POST");
 
-  // Grab data from payload
+  // Grab payload
   const payload = req.body;
-  const rows = payload.rows;
+  const table = payload.string;
 
-  // Update database
-  for (const row of rows) {
-    await supabase.from("PollVotes").insert({option_name : row.option_name, votes: 0, lobby_id : row.lobby_id});
-  }
-
-  res.status(200).send("Success");
+  const { data, error } = await supabase.from(table).select("*");
+  res.status(200).json(data, error);
 }
