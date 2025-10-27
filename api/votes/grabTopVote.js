a5bd046b9b74ec5cb410202bfe8bea6606e5568f
Tyler Leonard <tylerc35@hotmail.co.uk>
2025-10-27 17:12:35 +0000

    Changed grabTopVote to return all votes for lobby rather than top

--- api/votes/grabTopVote.js
+++ api/votes/grabTopVote.js
@@ -13,6 +13,6 @@ export default async function handler(req, res) {
   const lobbyCode = payload.lobby_id;
   
   const { data, error } = await supabase.from("PollVotes").select('option_name, votes').eq('lobby_id', lobbyCode).order('votes', { ascending: false });
-  res.status(200).json(data[0], error);
+  res.status(200).json(data, error);
   
 }
---
+++ htmlPresets.js
@@ -0,0 +1,8 @@
+let option_name;
+let option_id;
+
+let buttonPreset =
+`<div class = "option">
+    <label for = "${option_name}" id = "${option_name}Label"></label>
+    <input type = "checkbox" id = "${option_name}Checkbox" name = "${option_name}" option_id = "${option_id}"></input>
+</div>`
