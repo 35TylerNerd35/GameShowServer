55db9f2e5c0efef76a54d1768074dc415627641d
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-10-28 09:31:45 +0000

    databaseRequest trial

---
+++ api/databaseRequest.js
@@ -0,0 +1,22 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+  // Grab lobby code from payload
+  const payload = req.body;
+  const {table_name, method, params} = req.body;
+
+  const command = "await supabase.from(`";
+  command += table_name;
+  command += "`.";
+  command += method;
+  command += "()";
+  command += params;
+
+  const { data, error } = new Function(command);
+  res.status(200).json(data, error);
+}
--- htmlPresets.js
+++
@@ -1,8 +0,0 @@
-let option_name;
-let option_id;
-
-let buttonPreset =
-`<div class = "option">
-    <label for = "${option_name}" id = "${option_name}Label"></label>
-    <input type = "checkbox" id = "${option_name}Checkbox" name = "${option_name}" option_id = "${option_id}"></input>
-</div>`
