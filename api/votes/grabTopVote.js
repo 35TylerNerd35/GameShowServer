8f8a9d44d193cb9093894a051596c3effebb4bb4
Leonard T (FCI) <30126459@students.southwales.ac.uk>
2025-10-29 14:47:46 +0000

    New strategy for sending payload

--- api/votes/grabTopVote.js
+++ api/votes/grabTopVote.js
@@ -13,9 +13,7 @@ export default async function handler(req, res) {
   const lobbyCode = payload.lobby_id;
   
   const { data, error } = await supabase.from("PollVotes").select('option_name, votes').eq('lobby_id', lobbyCode).order('votes', { ascending: false });
-  let returnString = `{"rows" : `;
-  returnString += JSON.stringify(data);
-  returnString += "}"
-  res.status(200).json(data[0], error);
+  let dataArray = [data[0], data[1], data[2]];
+  res.status(200).json(dataArray, error);
   
 }
