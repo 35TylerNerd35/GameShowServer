4d2654ed15d9c82852529098e73714fb010a67b5
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-10-29 14:25:08 +0000

    Test Endpoint

---
+++ api/test.js
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+//   if (req.method != "POST") return res.status(405).send("Method not allowed, please use POST");
+
+  // Grab lobby code from payload
+//   const payload = req.body;
+//   const lobbyCode = payload.lobby_id;
+  
+  const { data, error } = await supabase.from("PollVotes").select('option_name, votes').eq('lobby_id', 18446).order('votes', { ascending: false });
+  res.status(200).json(data, error);
+  
+}
