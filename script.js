600bae08b62d7546c45d0ecf38b7b2d91670c863
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-11-27 15:07:27 +0000

    Removed timestamp for testing

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ async function SetupButtons() {
 
 async function OnToggleButtonClicked(toggleId)
 {
-    await supabase.from(toggleTable).update({toggle_id : toggleId}, {timestamp : new Date()});
+    await supabase.from(toggleTable).update({toggle_id : toggleId});
 }
 
 function CreateButton(buttonInformation) {
