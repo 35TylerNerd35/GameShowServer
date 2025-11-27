a0ffd609a524a342bbaaa2768779daf7459f0404
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-11-27 15:09:43 +0000

    Set API information as env

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "@supabase/supabase-js";
 
-// Connect to supabase
-const SUPABASE_URL = 'https://izghwhppfuumvzjehfrr.supabase.co'
-const SUPABASE_ANON_KEY = "sb_publishable_OfDnBziyMsBman1rO6HAuQ_5Oe1uBAf";
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
 // Declare vars
 const voteTable = 'PollVotes'
