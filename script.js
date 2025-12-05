ad11298202aa6adaadc38f4c78505230e9598bd4
Leonard T (FCI) <tylerc35@hotmail.co.uk>
2025-12-05 18:23:16 +0000

    New timer

--- script.js
+++ script.js
@@ -175,22 +175,15 @@ function RandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+let time = 30;
 function StartTimer()
 {
     // Update the count down every 1 second
     var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-        
-    // Find the distance between now and the count down date
-    var distance = (timestamp+30) - now;
-        
-    // Time calculations for days, hours, minutes and seconds
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000) + 30;
         
     // Output the result in an element with id="demo"
-    document.getElementById("timer").innerHTML = seconds + "seconds remaining";
+    document.getElementById("timer").innerHTML = time + "seconds remaining";
+    seconds -= 1;
         
     // If the count down is over, write some text 
     if (seconds < 0) {
